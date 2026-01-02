e385b394000c496a147cbb9e154f2388bf183adc
Dhruvil <dhruvilpatel.m@gmail.com>
2026-01-02 23:37:19 +0530

    feat: Add tRPC API route handler and a client-side page to consume it.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import { caller } from "@/trpc/server";
+'use client';
 
-const Page = async () => {
-  const users = await caller.getUsers();
-  return (
-    <div>
-      {JSON.stringify(users)}
-    </div>
-  )
-}
+import { useTRPC } from '@/trpc/client';
+import { useQuery } from '@tanstack/react-query';
+
+const Page = () => {
+  const trpc = useTRPC();
+
+  const {
+    data,
+    isLoading,
+    error,
+  } = useQuery(trpc.getUsers.queryOptions());
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
+
+  return <div>{JSON.stringify(data)}</div>;
+};
 
 export default Page;
