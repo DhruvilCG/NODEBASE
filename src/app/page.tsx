20eb9b4bd830b8265ad6d06c30d521d76d7d3c25
Dhruvil <dhruvilpatel.m@gmail.com>
2026-01-02 23:08:01 +0530

    feat: Implement tRPC server setup with a `getUsers` endpoint and display users on the root page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
-import prisma from "@/lib/db";
+import { caller } from "@/trpc/server";
 
 const Page = async () => {
-  const users = await prisma.user.findMany();
+  const users = await caller.getUsers();
   return (
     <div>
-      <Button variant={"outline"}>Click Me</Button>
       {JSON.stringify(users)}
     </div>
   )
