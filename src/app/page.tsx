6cd6fc46bbd9ae799822b6a5b2682d970a775630
Dhruvil <dhruvilpatel.m@gmail.com>
2026-01-02 20:22:41 +0530

    feat: Add initial page to display users and set up Prisma DB client.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-const Page = () => {
-    return (
-        <div>
-          <Button variant={"outline"}>Click Me</Button>
-        </div>
-    )
+const Page = async () => {
+  const users = await prisma.user.findMany();
+  return (
+    <div>
+      <Button variant={"outline"}>Click Me</Button>
+      {JSON.stringify(users)}
+    </div>
+  )
 }
 
-export default Page ;
+export default Page;
