04ba49a9d0f330d4db96e1218e8fbf31d219c4c5
Dhruvil <dhruvilpatel.m@gmail.com>
2026-01-04 00:24:40 +0530

    feat: Introduce `better-auth` for authentication, including API routes and a session display on the homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+"use client";
 
-const Page = async () => {
-  const queryClient = getQueryClient();
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions()) ;
+const Page = () => {
+  const { data } = authClient.useSession()
 
   return (
     <div>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Client/>
-        </Suspense>
-      </HydrationBoundary>
+      {JSON.stringify(data)}
+      {data &&
+        <Button onClick={() => authClient.signOut()}>
+          Logout
+        </Button>
+      }
     </div>
   )
 };
