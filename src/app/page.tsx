b2be97ed3cd0c882e17c755deaf383c55c11ceef
Dhruvil <dhruvilpatel.m@gmail.com>
2026-01-04 00:59:25 +0530

    feat: Set up tRPC with protected procedures, add authentication utilities, and a protected example page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
+import { LogoutButton } from "./logout";
 
 const Page = async () => {
-  await requireAuth(); 
+  await requireAuth();
+
+  const data = await caller.getUsers();
 
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
+    <div className="min-h-screen min-w-screen flex flex-col items-center justify-center gap-y-6">
       protected server component
+      <div>
+        {JSON.stringify(data, null, 2)}
+      </div>
+      <LogoutButton />
     </div>
   )
 };
