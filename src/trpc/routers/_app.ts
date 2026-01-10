86bd5470ad8f04c9e0b807abc0057d880d3a4e46
Dhruvil <dhruvilpatel.m@gmail.com>
2026-01-10 23:38:26 +0530

    feat: Implement workflows router with CRUD operations and update database schema for Workflows

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,31 +1,8 @@
-import { resolve } from 'path';
-import { baseProcedure, createTRPCRouter, premiumProcedure, protectedProcedure } from '../init';
-import prisma from '@/lib/db';
-import { inngest } from '@/inngest/client';
-import { google } from '@ai-sdk/google';
-import {generateText} from 'ai';
+import { createTRPCRouter } from '../init';
+import { workflowsRouter } from '@/features/workflows/server/routers';
 
 export const appRouter = createTRPCRouter({
-    testAi: premiumProcedure.mutation(async () => {
-        await inngest.send({
-            name: "execute/ai",
-        })
-
-        return { success: true, message: "Job queued" };
-    }),
-    getWorkflows: protectedProcedure.query(({ ctx }) => {
-        return prisma.workflows.findMany()
-    }),
-    createWorkflow: protectedProcedure.mutation(async () => {
-        await inngest.send({
-            name: "test/hello.world",
-            data: {
-                email: "test@example.com",
-            }
-        });
-
-        return { success: true, message: "Job queued" };
-    })
+    workflows: workflowsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
