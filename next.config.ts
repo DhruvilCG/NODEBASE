a13d7f8c83009bc68662fdf6f47aa6a9f97b1ba2
Dhruvil <dhruvilpatel.m@gmail.com>
2026-01-18 19:19:20 +0530

    chore: update Sentry organization in configuration

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,7 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: "yd00102",
+  org: "coding-gita",
 
   project: "autoflow",
 
